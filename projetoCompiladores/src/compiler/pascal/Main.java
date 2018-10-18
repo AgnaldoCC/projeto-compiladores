847bbe8249435a9e7df7ee4a7206900a2648a33e
AgnaldoCC <agnaldo.junior@ccc.ufcg.edu.br>
2018-10-18 20:11:45 -0300

    Integração com o CUP feita, faltando apenas fazer a gramática

---
+++ projetoCompiladores/src/compiler/pascal/Main.java
@@ -0,0 +1,25 @@
+package compiler.pascal;
+
+import java.io.FileReader;
+import java.nio.file.Paths;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String rootPath = Paths.get("").toAbsolutePath().toString();
+        String subPath = "/src/compiler/pascal/";
+
+        String sourcecode = rootPath + subPath + "program.pas";
+
+
+        try {
+            Parser p = new Parser(new LexicalAnalyzer(new FileReader(sourcecode)));
+            Object result = p.parse().value;
+            System.out.println("Compilacao concluida com sucesso...");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
